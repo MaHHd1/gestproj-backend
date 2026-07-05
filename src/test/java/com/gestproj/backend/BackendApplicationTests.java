a57716d21eafcb449b755b93fd9af5475504f7af
mahdi <mehdilakhoua123@gmail.com>
2026-07-05 15:51:42 +0100

    Initialisation projet Spring Boot

---
+++ src/test/java/com/gestproj/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestproj.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
